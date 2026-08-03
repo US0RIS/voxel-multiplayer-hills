167051e8b91f0a0539ca90d96cf3c53abd02de8d
US0RIS <emmett.dubeck@icloud.com>
2026-08-03 13:25:42 +0200

    Delete docs directory

--- docs/config.js
+++
@@ -1,6 +0,0 @@
-/* Voxel Multiplayer Hills v4.2.1 deployment configuration. */
-window.VOXEL_CONFIG = Object.freeze({
-  VERSION: '4.2.1',
-  PUBLIC_WEBSOCKET_URL: 'wss://voxel-multiplayer-hills-410-server.onrender.com/ws',
-  LOCAL_WEBSOCKET_URL: 'ws://localhost:8131/ws'
-});
